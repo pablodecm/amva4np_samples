daa5799582d38b0e94cbaee79977ba1d679f4c93
Pablo de Castro <pablodecm@gmail.com>
2016-08-22 17:08:02 +0200

    add QCD 4b sample from Oxford Summer campaign

---
+++ python/oxford_summer_2016.py
@@ -0,0 +1,28 @@
+
+
+eos_public = "root://eospublic.cern.ch/"
+amva4np_data = "/eos/experiment/amva4np/data/"
+campaign = "oxford_summer_2016/" 
+eos_dir = "".join([eos_public,amva4np_data,campaign])
+sample_suffix = "_{}_to_{}.root" 
+
+delphes = {}
+delphes["QCD_pp_bbbb_13TeV"] = {}
+
+delphes["QCD_pp_bbbb_13TeV"]["dir"] = "".join([
+    eos_dir,
+    "background/QCD/4b/MG5_pp_bbbb_13TeV_10M/"
+])
+delphes["QCD_pp_bbbb_13TeV"]["base_name"] = "pp_4b_13TeV_10M_py8"
+delphes["QCD_pp_bbbb_13TeV"]["file_name"] = "".join([
+    delphes["QCD_pp_bbbb_13TeV"]["dir"],
+    "Delphes_output/",
+    delphes["QCD_pp_bbbb_13TeV"]["base_name"]+"/",
+    delphes["QCD_pp_bbbb_13TeV"]["base_name"]+sample_suffix
+])
+delphes["QCD_pp_bbbb_13TeV"]["files"] = [
+    delphes["QCD_pp_bbbb_13TeV"]["file_name"].format(i*100000,(i+1)*100000) for i in range(100)
+]
+
+
+
